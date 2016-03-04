93d1af007232e58c20541f933476ddceec0e8dcc
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-03-04 17:41:43 +0000

    Correct string example

--- examples.js
+++ examples.js
@@ -2,4 +2,4 @@ var isiterable = require('./index.js');
 
 isiterable([1, 2, 3]); // returns true
 
-isiterable('hello i am a string'); // returns false
+isiterable('hello i am a string'); // returns true
