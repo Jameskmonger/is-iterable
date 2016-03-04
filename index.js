5a2965acf3a18a44045d391f5cad5b28d3513d07
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-03-04 13:50:14 +0000

    All tests pass

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function(mod) {
     mod.exports = function (obj) {
-        
+        return (obj !== undefined && obj !== null && typeof obj[Symbol.iterator] === 'function');
     };
 })(module);
