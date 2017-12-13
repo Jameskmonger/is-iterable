7778a9e0ac967acd75190625384200044c2e8ecc
chocolateboy <chocolate@cpan.org>
2017-12-13 19:39:42 +0000

    avoid ReferenceError: Symbol is not defined

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function(mod) {
     mod.exports = function (obj) {
-        return (obj !== undefined && obj !== null && typeof obj[Symbol.iterator] === 'function');
+        return (obj != null && typeof Symbol !== 'undefined' && Symbol && typeof obj[Symbol.iterator] === 'function');
     };
 })(module);
